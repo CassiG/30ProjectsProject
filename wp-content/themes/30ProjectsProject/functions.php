706f09b28e3f9ca9d7737ce87518e9af0eabe534
Cassi Gallagher <ccgall@gmail.com>
2017-10-15 16:17:59 -0700

    Add widgets functions

---
+++ wp-content/themes/30ProjectsProject/functions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Twenty Fifteen functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @link https://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * {@link https://codex.wordpress.org/Plugin_API}
+ *
+ * @package WordPress
+ * @subpackage 30 Projects Project
+ * @since 30 Projects Project 1.0
+ */
+
+function thirtyprojectsproject_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Widget Area', 'thirtyprojectsproject_widgets_init' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Add widgets here to appear in your sidebar.', 'thirtyprojectsproject_widgets_init' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'thirtyprojectsproject_widgets_init' );
